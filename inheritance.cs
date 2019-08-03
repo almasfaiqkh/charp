c77844feeb88f4f9fd5fb6d30f597e88cd2b2cfc
almasfaiqkh <44227540+almasfaiqkh@users.noreply.github.com>
2019-08-03 10:29:23 +0700

    Create inheritance.cs

---
+++ inheritance.cs
@@ -0,0 +1,90 @@
+using System;
+
+namespace Inheritance
+{
+    class Kucing
+    {
+        private string nama, ras;
+        private int umur;
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public string Ras
+        {
+            get { return ras; }
+            set { ras = value; }
+        }
+
+        public int Umur
+        {
+            get { return umur; }
+            set { umur = value; }
+        }
+    }
+}
+
+namespace Inheritance
+{
+    class Sphynx : Kucing
+    {
+        public Sphynx()
+        {
+            Nama = "Kucing Sphynx";
+            Ras = "Sphynx";
+            Umur = 4;
+        }
+    }
+
+    class Persia : Kucing
+    {
+        public Persia()
+        {
+            Nama = "Kucing persia";
+            Ras = "Persia";
+            Umur = 3;
+        }
+    }
+
+    class Himalaya : Kucing
+    {
+        public Himalaya()
+        {
+            Nama = "Kucing Himalaya";
+            Ras = "Himalaya";
+            Umur = 2;
+        }
+    }
+}
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sphynx sphynx = new Sphynx();
+            Persia persia = new Persia();
+            Himalaya himalaya = new Himalaya();
+
+            Console.WriteLine("Nama : {0} ", sphynx.Nama);
+            Console.WriteLine("Ras : {0} ", sphynx.Ras);
+            Console.WriteLine("Umur : {0} Tahun", sphynx.Umur);
+            Console.WriteLine();
+
+            Console.WriteLine("Nama : {0} ", persia.Nama);
+            Console.WriteLine("Ras : {0} ", persia.Ras);
+            Console.WriteLine("Umur : {0} Tahun", persia.Umur);
+            Console.WriteLine();
+
+            Console.WriteLine("Nama : {0} ", himalaya.Nama);
+            Console.WriteLine("Ras : {0} ", himalaya.Ras);
+            Console.WriteLine("Umur : {0} Tahun", himalaya.Umur);
+
+            Console.ReadKey();
+        }
+    }
+}
